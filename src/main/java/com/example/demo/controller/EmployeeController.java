aa49dacd4952fd28844133259f3558aed55e0caa
Yogesh <Yogesh@DESKTOP-GCQAQLS>
2020-07-06 16:57:42 +0530

    fifth commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class EmployeeController {
 
-	
-	
+	@RequestMapping("/shiv")
+	public String name()
+	{
+		return "shiv";
+	}
 	
 }
