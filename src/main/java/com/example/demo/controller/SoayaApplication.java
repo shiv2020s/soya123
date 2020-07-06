528293d78e00b7fcf1ce58c1762fbae6755711fa
Yogesh <Yogesh@DESKTOP-GCQAQLS>
2020-07-06 16:48:06 +0530

    fourth commit

---
+++ src/main/java/com/example/demo/controller/SoayaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoayaApplication.class, args);
+		System.out.println("shiv is my guru ");
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/SoayaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoayaApplication.class, args);
+		System.out.println("shiv is my guru ");
+	}
+
+}
